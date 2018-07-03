f23a8d6b48d2ae52846bdd893188939a8cb91941
IT201617 <IT201617@192.168.1.3>
2018-07-03 23:06:43 +0900

    コメント追加

--- Main/src/calcapp/logic/CalcLogic.java
+++ Main/src/calcapp/logic/CalcLogic.java
@@ -1,11 +1,12 @@
 package calcapp.logic;
 
 public class CalcLogic {
-
+	//足し算の処理
 	public static int tasu(int a,int b) {
 		return(a+b);
 
 	}
+	//引き算の処理
 	public static int hiku(int a, int b) {
 		return(a - b);
 	}
